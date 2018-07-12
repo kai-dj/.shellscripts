0812a0c9c051c4e37ec0a7d975220fac281b8353
kai-dj <kai@kai.dj>
2018-07-12 19:18:29 +0200

    add script date-insert

---
+++ date-insert.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# inserts date via xdotool
+
+# if german keyboard setxkbmap -layout de - special characters are displayed incorrectly if not
+setxkbmap -layout de -variant nodeadkeys &&
+# type date
+xdotool type $(date +%Y-%m-%d_%H:%M)
