545be38b2b0e2919db7e5abbec06c8ead2aa0c44
kai-dj <kai@kai.dj>
2018-07-12 19:41:11 +0200

    add script mix/remove_wine_context_menus

---
+++ mix/remove_wine_context_menus.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# remove wine applications from context menus etc.
+
+rm -f ~/.local/share/mime/packages/x-wine*;
+rm -f ~/.local/share/applications/wine-extension*;
+rm -f ~/.local/share/icons/hicolor/*/*/application-x-wine-extension*;
+rm -f ~/.local/share/mime/application/x-wine-extension*;
+
+
