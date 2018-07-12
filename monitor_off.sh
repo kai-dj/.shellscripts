6ea946198b642f46fb46515833efdf7c72b99cb6
kai-dj <kai@kai.dj>
2018-07-12 18:58:30 +0200

    add script monitor_off.sh

---
+++ monitor_off.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# wait a second, then turn off monitor
+sleep 1 && xset -display :0.0 dpms force off
+# turn off numlock if numlockx is available
+if [ -x "$(command -v numlockx)" ]; then
+    numlockx off;
+fi
