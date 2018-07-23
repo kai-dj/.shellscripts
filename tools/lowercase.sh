062fe75d90812fb742bba95a84ed5b4874399123
kai-dj <kai@kai.dj>
2018-07-23 12:43:12 +0200

    add script tools/lowercase

---
+++ tools/lowercase.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# renames file to lowercase (could harm files - be careful)
+
+
+for file in "$@"
+do
+    folder=$(dirname "$(realpath "$file")")
+    filename=$(basename "$file")
+    lowerfilename=$( echo "$filename" | tr [:upper:] [:lower:])
+    mv -v "$folder""/""$filename" "$folder""/""$lowerfilename"
+done
