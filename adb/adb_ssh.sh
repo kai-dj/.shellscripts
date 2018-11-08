6ea63121a66a1868a12da541e46c86191bae3ea1
kai-dj <kai@kai.dj>
2018-11-08 16:00:43 +0100

    add script adb/adb_ssh.sh (connecting to termux ssh via adb)

---
+++ adb/adb_ssh.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# access termux via ssh over adb also forward httpd port 8080
+# tldr; for https://glow.li/technology/2016/9/20/access-termux-via-usb/
+adb forward tcp:8022 tcp:8022 &&
+    adb forward tcp:8080 tcp:8080 &&
+    ssh localhost -p 8022
