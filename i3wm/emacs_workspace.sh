b8ef32e46cc098b57324fae5aced75f8dca8e21b
kai-dj <kai@kai.dj>
2018-11-08 16:02:22 +0100

    add script i3wm/emacs_workspace.sh

---
+++ i3wm/emacs_workspace.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#open emacs workspace with 2 instances and placeholder for speedbar (via speedkill)
+
+i3-msg "workspace emacs; append_layout ~/.config/i3/workspaces/emacs_speed.json" &&
+(emacsclient -create-frame --alternate-editor="emacs"&) &
+(sleep 1 && emacsclient -create-frame --alternate-editor="emacs"&) &
+(sleep 2 && xdotool click 1&) &
+#        i3-msg "move left,move left,move left,focus left,kill"
+#        &&i3-msg "focus right")&
