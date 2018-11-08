57bec5ccb8a7407c4b4a9c7ed88eb4a663fdd8b7
kai-dj <kai@kai.dj>
2018-11-08 15:11:54 +0100

    added "/" at end of target directory for safety

--- mix/clean-home.sh
+++ mix/clean-home.sh
@@ -4,4 +4,4 @@ shopt -s extglob
 dirname=$(date +%Y-%m-%d_%H:%M)
 mkdir ~/tmp/"$dirname"
 cd ~
-mv -v !(.*|h6x|dwn|mnt|org|prj|tmp|WRM|README.org) ~/tmp/"$dirname"
+mv -v !(.*|h6x|dwn|mnt|org|prj|tmp|WRM|README.org) ~/tmp/"$dirname"/
