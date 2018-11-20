85d17fa49143371878f592f2bc101c5d2e9903f8
kai-dj <kai@kai.dj>
2018-11-20 14:46:59 +0100

    add script adb/adb_volume+ and adb/adb_volume-

---
+++ adb/adb_volume+.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# sends volume up to adb device
+adb shell input keyevent KEYCODE_VOLUME_UP
---
+++ adb/adb_volume-.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# sends volume down to adb device
+adb shell input keyevent KEYCODE_VOLUME_DOWN
