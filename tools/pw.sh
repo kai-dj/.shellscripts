e378fa2e8a40bdc9b93ea93231c1236ba38e3f69
Kai Lüking <kai@kai.dj>
2019-09-04 19:38:09 +0200

    ADD - aliased screenshot_selection_to_clipboard + DEL - unecessary non-aliased pw

--- tools/pw.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# generate random password
-if (( $# != 1 )); then 
-  date +%s | sha256sum | base64 | head -c 32; echo; 
-else 
-  date +%s | sha256sum | base64 | head -c "$@"; echo; 
-fi
---
+++ tools/screenshot_selection_to_clipboard.a.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#script for taking screenshot with scrot to clipboard
+TEMP_PNG=$(mktemp --suffix=.png)
+# temp file must be deleted, because scrot won't overwrite existing files :/
+rm -f "$TEMP_PNG"
+scrot -s "$TEMP_PNG"
+xclip -selection clipboard -t image/png -i "$TEMP_PNG"
+#do not delete file, relay on tmp beeing cleaned at reboot
+#rm -f "$TEMP_PNG"
