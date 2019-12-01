de9af314280e29950fd1f4b12061b84029c6395f
Kai Lüking <kai-frederik.lueking@letsdev.de>
2019-12-01 15:10:34 +0100

    DOC

--- adb/adb_intent_ssh.a.sh
+++ adb/adb_intent_ssh.a.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-# access termux via ssh over adb also forward httpd port 8080
-# tldr; for https://glow.li/technology/2016/9/20/access-termux-via-usb/
+# trigger net.dinglish.tasker.ssh intent (wich triggers tasker ssh enable script)
 adb shell am broadcast --user 0 -a net.dinglish.tasker.ssh
