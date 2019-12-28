e231ace0bb4894b3fa83763afdb6eb2d17066568
Kai Lüking <kai-frederik.lueking@letsdev.de>
2019-12-28 23:38:45 +0100

    FIX - change swipe position

--- adb/adb_unlock.a.sh
+++ adb/adb_unlock.a.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # unlocks screen of android device
 adb shell input keyevent 82 &&
-    adb shell input swipe 800 400 800 800 &&
+    adb shell input swipe 800 200 800 1000 &&
     adb shell input text $(cat ~/.pw/android|cut -d":" -f2)
 
 #enter not needed for my device
