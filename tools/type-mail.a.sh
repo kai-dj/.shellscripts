6027a7a195e463c9a0bf3bcf0559cad388c63416
Kai Lüking <kai-frederik.lueking@letsdev.de>
2019-12-28 23:41:13 +0100

    ADD - aliased script to type email address

---
+++ tools/type-mail.a.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+DESCRIPTION="Script using xdotool to type email address."
+
+if [ "$1" = "-h" ] || [ "$1" = "--help" ]
+ then
+  echo "$DESCRIPTION"
+  exit 0
+fi
+
+xdotool type "kai.lueking" &&
+xdotool key U0040 &&
+xdotool type "gmail.com"
+
