779e719d86bb85ce156f7001e913d509f63e8461
Kai Lüking <kai-frederik.lueking@letsdev.de>
2021-07-27 17:11:02 +0200

    SMALLFIX

--- environment.sh
+++ environment.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
+# shellcheck disable=SC2154
 export ALTERNATE_EDITOR="emacs"
 export EDITOR="emacsclient -t"                  # $EDITOR opens in terminal
 export VISUAL="emacsclient -c -a emacs"         # $VISUAL opens in GUI mode
 export ANDROID_HOME=/opt/android-sdk
-export PATH=$PATH:/home/aik/.config/aik/linux/bin:/opt/android-sdk/tools/:/opt/android-sdk/platform-tools/:/usr/lib/eclipse/:$HOME/.node_modules/bin:/home/aik/bin
+export PATH=$PATH:"$HOME"/
+export PATH=$PATH:"$HOME"/bin
+export PATH=$PATH:"$HOME"/.shellscripts/bin
+export PATH=$PATH:"$HOME"/.config/aik/linux/bin
+export PATH=$PATH:"$HOME"/.node_modules/bin
+export PATH=$PATH:"$HOME"/.local/bin
+export PATH=$PATH:/usr/lib/eclipse
+export PATH=$PATH:/opt/android-sdk/tools
+export PATH=$PATH:/opt/android-sdk/platform-tools
 export npm_config_prefix=~/.node_modules
 
 export ANDROID_SDK=/opt/android-sdk
