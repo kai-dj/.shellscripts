8908580db15b6aacb9c1e25ce5f981ccd86d04e0
Kai Lüking <kai-frederik.lueking@letsdev.de>
2021-07-27 17:31:27 +0200

    UPD - servedir with upload

--- tools/servedir.a.sh
+++ tools/servedir.a.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #serve directory and show local ip
 localip=$(ip route get 1.2.3.4|cut -d " " -f7 | xargs)
-port=9090
-echo "serving ""$(pwd)"" on ""$localip"":""$port"
-python2 -m SimpleHTTPServer $port
+#TODO port
+#port=9090
+echo "serving ""$(pwd)"" on ""$localip" #":""$port"
+#python2 -m SimpleHTTPServer $port
+python3 SimpleHTTPServerWithUpload.py
