93921ae9891b42f3d0d26b6a4de8bb9200e31ea1
Kai Lüking <kai-frederik.lueking@letsdev.de>
2022-01-02 18:03:27 +0100

    FIX - smallfix

--- tools/type-mail.a.sh
+++ tools/type-mail.a.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 DESCRIPTION="Script using xdotool to type email address."
-
+beep
 if [ "$1" = "-h" ] || [ "$1" = "--help" ]
  then
   echo "$DESCRIPTION"
-  exit 0
+  #exit 0
 fi
 
 xdotool type "kai.lueking" &&
 xdotool key U0040 &&
-xdotool type "monday-consulting.com"
+xdotool type "gmail.com"
+#xdotool type "monday-consulting.com"
