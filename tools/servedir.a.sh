b80515450bec014dca99356cb7199bc7c37f33c8
Kai Lüking <kai-frederik.lueking@letsdev.de>
2022-01-02 18:01:56 +0100

    FIX - update serve-script

--- tools/servedir.a.sh
+++ tools/servedir.a.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #serve directory and show local ip
 localip=$(ip route get 1.2.3.4|cut -d " " -f7 | xargs)
 #TODO port
 #port=9090
 echo "serving ""$(pwd)"" on ""$localip" #":""$port"
 #python2 -m SimpleHTTPServer $port
-python3 SimpleHTTPServerWithUpload.py
+python3 ~/.shellscripts/tools/SimpleHTTPServerWithUpload.py .
